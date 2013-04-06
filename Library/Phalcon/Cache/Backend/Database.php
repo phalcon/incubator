28dd04820344e897bcf327b4c7e5f38946425f5d
phalcon <andres@phalconphp.com>
2013-04-05 18:59:13 -0600

    Adding Database adapter for Acl

--- Library/Phalcon/Cache/Backend/Database.php
+++ Library/Phalcon/Cache/Backend/Database.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
+  +------------------------------------------------------------------------+
+  | Phalcon Framework                                                      |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2011-2012 Phalcon Team (http://www.phalconphp.com)       |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file docs/LICENSE.txt.                        |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to license@phalconphp.com so we can send you a copy immediately.       |
+  +------------------------------------------------------------------------+
+  | Authors: Andres Gutierrez <andres@phalconphp.com>                      |
+  |          Eduar Carvajal <eduar@phalconphp.com>                         |
+  +------------------------------------------------------------------------+
+*/
+
 namespace Phalcon\Cache\Backend;
 
 use Phalcon\Db,
@@ -7,6 +25,11 @@ use Phalcon\Db,
 	Phalcon\Cache\BackendInterface,
 	Phalcon\Cache\Exception;
 
+/**
+ * Phalcon\Cache\Backend\Database
+ *
+ * This backend uses a database as cache backend
+ */
 class Database extends Backend implements BackendInterface
 {
 
