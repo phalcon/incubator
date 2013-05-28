e9ced7375e1407decc0baa024426012f9f467693
Alexey Stetsenko <freekzy@gmail.com>
2013-05-28 08:34:07 +0300

    Adding Yaml adapter for Phalcon\Config

---
+++ Library/Phalcon/Config/Adapter/Yaml.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Phalcon\Config\Adapter;
+
+use Phalcon\Config,
+	Phalcon\Config\Exception;
+
+/**
+ * Phalcon\Config\Adapter\Yaml
+ *
+ * Reads yaml files and convert it to Phalcon\Config objects.
+ */
+class Yaml extends Config implements \ArrayAccess
+{
+
+	/**
+	 * Phalcon\Config\Adapter\Yaml
+	 *
+	 * @param string $filePath
+	 */
+	public function __construct($filePath, $callbacks = array())
+	{
+		if (!extension_loaded('yaml'))
+			throw new Exception('Yaml extension not loaded');
+
+		if (false === $result =@yaml_parse_file($filePath, 0, $ndocs, $callbacks))
+			throw new Exception('Configuration file ' . $filePath . ' can\'t be loaded');
+
+		parent::__construct($result);
+	}
+
+}
