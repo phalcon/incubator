8c2245630e438c6f8dd5c4ba498d915f19a6fa06
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-07-28 22:53:43 +0300

    Update Slug.php

--- Library/Phalcon/Utils/Slug.php
+++ Library/Phalcon/Utils/Slug.php
@@ -44,21 +44,20 @@ class Slug
             throw new \Phalcon\Exception('iconv module not loaded');
         }
 
-        // Save the old locale
-        $oldLocale = setlocale(LC_ALL, 0);
+        // Save the old locale and set the new locale to UTF-8
+        $oldLocale = setlocale(LC_ALL, 'en_US.UTF-8');
 
-        // Set the locale to UTF8
-        setlocale(LC_ALL, 'en_US.UTF-8');
+        $clean = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
 
         if( !empty($replace) )
         {
-            $string = str_replace((array) $replace, ' ', $string);
+            $clean = str_replace((array) $replace, ' ', $clean);
         }
 
-        $clean = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
         $clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
-        $clean = strtolower(trim($clean, '-'));
+        $clean = strtolower($clean);
         $clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
+        $clean = trim($clean, $delimiter);
 
         // Revert back to the old locale
         setlocale(LC_ALL, $oldLocale);
