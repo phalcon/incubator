12af0f90868b2631e9e231b6e43c17d0b15a4e3b
Nikita Vershinin <endeveit@gmail.com>
2013-09-21 22:15:33 +0600

    Paginator navigation menu renderer

---
+++ Library/Phalcon/Paginator/Pager/Range.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Phalcon Framework
+ *
+ * This source file is subject to the New BSD License that is bundled
+ * with this package in the file docs/LICENSE.txt.
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@phalconphp.com so we can send you a copy immediately.
+ *
+ * @author Nikita Vershinin <endeveit@gmail.com>
+ */
+namespace Phalcon\Paginator\Pager;
+
+use Phalcon\Paginator\Pager;
+
+/**
+ * \Phalcon\Paginator\Pager\Range
+ *
+ * Base class for ranges objects.
+ */
+abstract class Range
+{
+
+    /**
+     * Pager object.
+     *
+     * @var \Phalcon\Paginator\Pager
+     */
+    protected $pager = null;
+
+    /**
+     * Window size.
+     *
+     * @var integer
+     */
+    protected $chunkLength = 0;
+
+    /**
+     * Class constructor.
+     *
+     * @param \Phalcon\Paginator\Pager $pager
+     * @param integer                  $chunkLength
+     */
+    public function __construct(Pager $pager, $chunkLength)
+    {
+        $this->pager       = $pager;
+        $this->chunkLength = abs(intval($chunkLength));
+
+        if ($this->chunkLength == 0) {
+            $this->chunkLength = 1;
+        }
+    }
+
+    /**
+     * Calculate and returns an array representing the range around the current page.
+     *
+     * @return array
+     */
+    abstract public function getRange();
+
+}
---
+++ Library/Phalcon/Paginator/Pager/Range/Sliding.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Phalcon Framework
+ *
+ * This source file is subject to the New BSD License that is bundled
+ * with this package in the file docs/LICENSE.txt.
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@phalconphp.com so we can send you a copy immediately.
+ *
+ * @author Nikita Vershinin <endeveit@gmail.com>
+ */
+namespace Phalcon\Paginator\Pager\Range;
+
+use Phalcon\Paginator\Pager\Range;
+
+/**
+ * \Phalcon\Paginator\Pager\Range\Sliding
+ *
+ * «Smooth» ranges, e.g.: when on
+ *  [1] [2] 3
+ *  next range will be:
+ *  [3] 4 [5]
+ */
+class Sliding extends Range
+{
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return array
+     */
+    public function getRange()
+    {
+        $page  = $this->pager->getCurrentPage();
+        $pages = $this->pager->getLastPage();
+
+        $chunk = $this->chunkLength;
+
+        if ($chunk > $pages) {
+            $chunk = $pages;
+        }
+
+        $chunkStart = $page - (floor($chunk / 2));
+        $chunkEnd   = $page + (ceil($chunk / 2) - 1);
+
+        if ($chunkStart < 1) {
+            $adjust     = 1 - $chunkStart;
+            $chunkStart = 1;
+            $chunkEnd   = $chunkEnd + $adjust;
+        }
+
+        if ($chunkEnd > $pages) {
+            $adjust     = $chunkEnd - $pages;
+            $chunkStart = $chunkStart - $adjust;
+            $chunkEnd   = $pages;
+        }
+
+        return range($chunkStart, $chunkEnd);
+    }
+
+}
