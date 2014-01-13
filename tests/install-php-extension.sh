df8237f64069081d227c8bcc010b1d7393a284a6
Nikita Vershinin <endeveit@gmail.com>
2014-01-13 19:24:58 +0600

    And again…

--- tests/install-php-extension.sh
+++ tests/install-php-extension.sh
@@ -3,4 +3,4 @@
 git clone -q https://github.com/phalcon/cphalcon.git -b 1.2.4
 cd cphalcon/build
 sudo ./travis-install
-echo "extension=phalcon.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+sudo make install && phpenv config-add ../unit-tests/ci/phalcon.ini
