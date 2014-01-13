b5151289a97f96757e47a656f8bad0c7918b7b26
Nikita Vershinin <endeveit@gmail.com>
2014-01-13 21:36:09 +0600

    Fixes DumpTest.php to work with PHP 5.3

--- tests/Phalcon/Debug/DumpTest.php
+++ tests/Phalcon/Debug/DumpTest.php
@@ -6,14 +6,15 @@ class DumpTest extends \PHPUnit_Framework_TestCase
     (
         'htmlString' => '<p>test</p>',
     );
-    
+
     // set for testing purposes
     protected $flushBuffer = false;
-    
+
     public function testDumpingHtmlStringVarByDefaultShouldEchoNonEscapedDump()
     {
         ob_start();
-        (new \Phalcon\Debug\Dump($this->flushBuffer))->dump($this->fixtures['htmlString']);
+        $dump = new \Phalcon\Debug\Dump($this->flushBuffer);
+        $dump->dump($this->fixtures['htmlString']);
         $output = ob_get_clean();
         $this->assertBacktraceExists($output);
         // assert string was not converted
@@ -21,14 +22,15 @@ class DumpTest extends \PHPUnit_Framework_TestCase
         // assert correct sapi set
         $this->assertEquals(PHP_SAPI, \Phalcon\Debug\Dump::getSapi());
     }
-    
+
     public function testDumpingHtmlStringVarWithNonCliSapiShouldEchoEscapedDump()
     {
         // non cli sapi
         \Phalcon\Debug\Dump::setSapi('apache');
-        
+
         ob_start();
-        (new \Phalcon\Debug\Dump($this->flushBuffer))->dump($this->fixtures['htmlString']);
+        $dump = new \Phalcon\Debug\Dump($this->flushBuffer);
+        $dump->dump($this->fixtures['htmlString']);
         $output = ob_get_clean();
         $this->assertBacktraceExists($output);
         // assert string was converted
@@ -36,14 +38,15 @@ class DumpTest extends \PHPUnit_Framework_TestCase
         // assert correct sapi set
         $this->assertEquals('apache', \Phalcon\Debug\Dump::getSapi());
     }
-    
+
     public function testDumpingHtmlStringVarWithSupressedOutputShouldReturnValue()
     {
         // supress output
         \Phalcon\Debug\Dump::setOutput(false);
-        
+
         ob_start();
-        $return = (new \Phalcon\Debug\Dump($this->flushBuffer))->dump($this->fixtures['htmlString']);
+        $dump = new \Phalcon\Debug\Dump($this->flushBuffer);
+        $return = $dump->dump($this->fixtures['htmlString']);
         $output = ob_get_clean();
         $this->assertBacktraceNotExists($output);
         // assert no output
@@ -55,11 +58,12 @@ class DumpTest extends \PHPUnit_Framework_TestCase
         // assert right output flag was set
         $this->assertFalse(\Phalcon\Debug\Dump::getOutput());
     }
-    
+
     public function testSecondDumpParamShouldOverrideGlobalOutputSetting()
     {
         ob_start();
-        $return = (new \Phalcon\Debug\Dump($this->flushBuffer))->dump($this->fixtures['htmlString'], false);
+        $dump = new \Phalcon\Debug\Dump($this->flushBuffer);
+        $return = $dump->dump($this->fixtures['htmlString'], false);
         $output = ob_get_clean();
         // no backtrace
         $this->assertBacktraceNotExists($output);
@@ -68,53 +72,54 @@ class DumpTest extends \PHPUnit_Framework_TestCase
         // assert dump was returned
         $this->assertContains($this->fixtures['htmlString'], $return);
     }
-    
+
     public function testIfNoXdebugVarDumpObjectShouldFallback()
     {
-        /* @var $mockDumpStub \PhalconDebug\Dump */
-        $mockDumpStub = $this->getMock('\Phalcon\Debug\Dump', array('xdebugDumpExists'), 
+        /* @var $mockDumpStub \Phalcon\Debug\Dump */
+        $mockDumpStub = $this->getMock('\Phalcon\Debug\Dump', array('xdebugDumpExists'),
             array($this->flushBuffer));
-        
+
         $mockDumpStub->expects($this->once())
             ->method('xdebugDumpExists')
             ->will($this->returnValue(false));
-        
+
         ob_start();
         $mockDumpStub->dump($this->fixtures['htmlString']);
-        $output = ob_get_clean(); 
+        $output = ob_get_clean();
         $this->assertBacktraceExists($output);
         // assert string was not converted
         $this->assertContains($this->fixtures['htmlString'], $output);
-        
+
     }
-    
+
     public function testObGetCleanShouldReturnEmptyIfFlushBufferNotSet()
     {
         ob_start();
-        $return =  (new \Phalcon\Debug\Dump())->dump($this->fixtures['htmlString']);
+        $dump = new \Phalcon\Debug\Dump();
+        $return = $dump->dump($this->fixtures['htmlString']);
         $output = ob_get_clean();
-        
+
         // assert output empty
         $this->assertEmpty($output);
         $this->assertBacktraceExists($return);
         $this->assertContains($this->fixtures['htmlString'], $return);
 
     }
-        
+
     protected function assertBacktraceExists($output)
     {
         // assert backtrace exists
         $this->assertContains('Line:', $output);
         $this->assertContains('DumpTest.php', $output);
     }
-    
+
     protected function assertBacktraceNotExists($output)
     {
         // assert backtrace doesnt exist
         $this->assertNotContains('Line:', $output);
         $this->assertNotContains('DumpTest.php', $output);
     }
-    
+
     protected function tearDown()
     {
         parent::tearDown();
@@ -122,6 +127,6 @@ class DumpTest extends \PHPUnit_Framework_TestCase
         \Phalcon\Debug\Dump::setOutput(true);
         // reset PHP_SAPI
         \Phalcon\Debug\Dump::setSapi(PHP_SAPI);
-        
+
     }
 }
