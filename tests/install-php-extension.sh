bd1d49651c28e70404eb3ce8ab81c681d446ee99
Nikita Vershinin <endeveit@gmail.com>
2014-01-13 19:06:53 +0600

    Invalid branch name

--- tests/install-php-extension.sh
+++ tests/install-php-extension.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 git clone -q https://github.com/phalcon/cphalcon.git
-git checkout 1.2.4
+git checkout origin/1.2.4
+git pull origin 1.2.4
 cd cphalcon/build
 sudo ./install && phpenv config-add ../../tests/phalcon.ini
