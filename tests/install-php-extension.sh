2c898aa95179c0ace4a1fa3abdad31ec2deaf34c
Nikita Vershinin <endeveit@gmail.com>
2014-01-13 19:14:29 +0600

    Trying to clone correct branch

--- tests/install-php-extension.sh
+++ tests/install-php-extension.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-git clone -q https://github.com/phalcon/cphalcon.git
-git checkout origin/1.2.4
-git pull origin 1.2.4
+git clone https://github.com/phalcon/cphalcon.git -b 1.2.4
 cd cphalcon/build
 sudo ./install && phpenv config-add ../../tests/phalcon.ini
