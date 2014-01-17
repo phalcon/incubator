7ef750e979bb4d2ae464c7dceb639ef4fda218fa
nexik <tomek@sabaki.pl>
2014-01-17 23:12:19 +0100

    Validator Db Uniqueness

---
+++ Library/Phalcon/Validation/Validator/Db/Uniqueness.php
@@ -0,0 +1,78 @@
+<?php
+namespace Phalcon\Validation\Validator\Db;
+
+use Phalcon\Validation\Validator;
+use Phalcon\Validation\ValidatorInterface;
+use Phalcon\Validation\Message;
+use Phalcon\Db\Adapter\Pdo as DbConnection;
+use Phalcon\Validation\Exception as ValidationException;
+
+class Uniqueness extends Validator implements ValidatorInterface
+{
+    /**
+     * Database connection
+     * @var Phalcon\Db\Adapter\Pdo
+     */
+    private $db;
+
+    public function __construct(array $options = array(), $db = null)
+    {
+        parent::__construct($options);
+
+        if (null === $db) {
+            // try to get db instance from default Dependency Injection
+            $di = \Phalcon\DI::getDefault();
+
+            if ($di instanceof \Phalcon\DiInterface && $di->has('db')) {
+                $db = $di->get('db');
+            }
+        }
+
+        if ($db instanceof DbConnection) {
+            $this->db = $db;
+        } else {
+            throw new ValidationException('Validator Uniquness require connection to database');
+        }
+
+        if (false === $this->isSetOption('table')) {
+            throw new ValidationException('Validator require table option to be set');
+        }
+
+        if (false === $this->isSetOption('column')) {
+            throw new ValidationException('Validator require column option to be set');
+        }
+    }
+
+    /**
+     * Executes the uniqueness validation
+     *
+     * @param Phalcon\Validation $validator
+     * @param string $attribute
+     * @return boolean
+     */
+    public function validate($validator, $attribute)
+    {
+        $table = $this->getOption('table');
+        $column = $this->getOption('column');
+
+        $result = $this->db->fetchOne(
+            sprintf('SELECT COUNT(*) as count FROM %s WHERE %s = ?', $table, $column),
+            \Phalcon\Db::FETCH_ASSOC,
+            array($validator->getValue($attribute))
+        );
+
+        if ($result['count']) {
+            $message = $this->getOption('message');
+
+            if (null === $message) {
+                $message = 'Already taken. Choose another!';
+            }
+
+            $validator->appendMessage(new Message($message, $attribute, 'Uniqueness'));
+
+            return false;
+        }
+
+        return true;
+    }
+}
