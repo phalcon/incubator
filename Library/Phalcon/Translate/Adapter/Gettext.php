e2d83346a4b366df7dbcf4a2c17d30a2305a0796
Nikita Vershinin <endeveit@gmail.com>
2014-01-23 21:46:51 +0600

    Fix for #121 (placeholders in gettext)

--- Library/Phalcon/Translate/Adapter/Gettext.php
+++ Library/Phalcon/Translate/Adapter/Gettext.php
@@ -91,10 +91,11 @@ class Gettext extends Adapter implements AdapterInterface
     public function query($index, $placeholders = null, $domain = null)
     {
         if ($domain === null) {
-            return gettext($index);
+            $translation = gettext($index);
+        } else {
+            $translation = dgettext($domain, $index);
         }
 
-        $translation = dgettext($domain, $index);
         if (is_array($placeholders)) {
             foreach ($placeholders as $key => $value) {
                 $translation = str_replace('%' . $key . '%', $value, $translation);
