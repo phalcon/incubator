5187238e3bfff59a48a74bc3388fd9bc896b6aa5
Yerlen Zhubangaliyev <yz@yz.kz>
2014-11-14 16:44:45 +0600

    initial

---
+++ Library/Phalcon/Translate/Adapter/Intl.php
@@ -0,0 +1,10 @@
+<?php
+namespace Phalcon\Translate\Adapter;
+
+use Phalcon\Translate\Adapter;
+use Phalcon\Translate\AdapterInterface;
+
+class Intl extends Adapter implements AdapterInterface
+{
+
+}
