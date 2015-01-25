58dc52ffc87fc6d048e948685c36831cbcea663e
Grigory Parshikov <grisha.mexanik@gmail.com>
2015-01-25 20:34:05 +0500

    Add card type validation

--- Library/Phalcon/Mvc/Model/Validator/CardNumber.php
+++ Library/Phalcon/Mvc/Model/Validator/CardNumber.php
@@ -3,6 +3,7 @@ namespace Phalcon\Mvc\Model\Validator;
 
 use Phalcon\Mvc\Model\Exception;
 use Phalcon\Mvc\ModelInterface;
+use Phalcon\Mvc\CollectionInterface;
 
 /**
  * Phalcon\Mvc\Model\Validator\CardNumber
@@ -32,9 +33,13 @@ use Phalcon\Mvc\ModelInterface;
  */
 class CardNumber extends \Phalcon\Mvc\Model\Validator
 {
+    const AMERICAN_EXPRESS  = 0; // 34, 37
+    const MASTERCARD        = 1; // 51-55
+    const VISA              = 2; // 4
+
     public function validate($record)
     {
-        if (false === is_object($record) || false === $record instanceof ModelInterface) {
+        if (false === $record instanceof ModelInterface && false === $record instanceof CollectionInterface) {
             throw new Exception('Invalid parameter type.');
         }
 
@@ -45,8 +50,39 @@ class CardNumber extends \Phalcon\Mvc\Model\Validator
         }
 
         $fieldValue = $record->readAttribute($field);
+        $value = preg_replace('/[^\d]/', '', $fieldValue);
 
-        $value = strrev(preg_replace('/[^\d]/', '', $fieldValue));
+        if ($this->isSetOption('type')) {
+            $type = $this->getOption('type');
+
+            $result = true;
+
+            switch ($type) {
+                case CardNumber::AMERICAN_EXPRESS:
+                    $issuer = substr($value, 0, 2);
+                    $result = (true === in_array($issuer, array(34, 37)));
+                    break;
+                case CardNumber::MASTERCARD:
+                    $issuer = substr($value, 0, 2);
+                    $result = (true === in_array($issuer, array(51, 52, 53, 54, 55)));
+                    break;
+                case CardNumber::VISA:
+                    $issuer = $value[0];
+                    $result = ($issuer == 4);
+                    break;
+                default:
+                    throw new Exception('Incorrect type specifier');
+            }
+
+            if (false === $result) {
+                $message = $this->getOption('message') ?: 'Credit card number is invalid';
+
+                $this->appendMessage($message, $field, "CardNumber");
+                return false;
+            }
+        }
+
+        $value = strrev($value);
         $checkSum = 0;
 
         for ($i = 0; $i < strlen($value); $i++) {
