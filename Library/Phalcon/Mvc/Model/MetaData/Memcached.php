9b556ae81b3c5f2f131f28f0eefdf902310a90e3
Dmitriy Zhavoronkov <dimaz.lark@gmail.com>
2015-02-05 17:21:36 +0300

    PSR-2 standard parenthesis fix

--- Library/Phalcon/Mvc/Model/MetaData/Memcached.php
+++ Library/Phalcon/Mvc/Model/MetaData/Memcached.php
@@ -1,14 +1,21 @@
 <?php
 /**
  * Phalcon Framework
+ *
  * This source file is subject to the New BSD License that is bundled
  * with this package in the file docs/LICENSE.txt.
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@phalconphp.com so we can send you a copy immediately.
  *
- * @author Nikita Vershinin    <endeveit@gmail.com>
- * @author Dmitriy Zhavoronkov <dimaz.lark@gmail.com>
+ * PHP version 5
+ *
+ * @category Phalcon
+ * @package  Phalcon\Mvc\Model\MetaData
+ * @author   Nikita Vershinin    <endeveit@gmail.com>
+ * @author   Dmitriy Zhavoronkov <dimaz.lark@gmail.com>
+ * @license  New BSD License
+ * @link     http://phalconphp.com/
  */
 namespace Phalcon\Mvc\Model\MetaData;
 
@@ -20,10 +27,16 @@ use Memcached as MemcachedGeneric;
 /**
  * \Phalcon\Mvc\Model\MetaData\Memcached
  * Memcached adapter for \Phalcon\Mvc\Model\MetaData
+ *
+ * @category Phalcon
+ * @package  Phalcon\Mvc\Model\MetaData
+ * @author   Nikita Vershinin    <endeveit@gmail.com>
+ * @author   Dmitriy Zhavoronkov <dimaz.lark@gmail.com>
+ * @license  New BSD License
+ * @link     http://phalconphp.com/
  */
 class Memcached extends Base
 {
-
     /**
      * Default option for memcached port.
      *
@@ -55,7 +68,7 @@ class Memcached extends Base
     /**
      * {@inheritdoc}
      *
-     * @param  null|array $options
+     * @param null|array $options options array
      *
      * @throws \Phalcon\Mvc\Model\Exception
      */
@@ -91,20 +104,22 @@ class Memcached extends Base
     protected function getCacheBackend()
     {
         if (null === $this->memcached) {
-            $this->memcached = new CacheBackend(new CacheFrontend(array('lifetime' => $this->options['lifetime'])),
+            $this->memcached = new CacheBackend(
+                new CacheFrontend(array('lifetime' => $this->options['lifetime'])),
                 array(
                     'servers' => array(
                         array(
-                            'host'   => $this->options['host'],
-                            'port'   => $this->options['port'],
+                            'host' => $this->options['host'],
+                            'port' => $this->options['port'],
                             'weight' => $this->options['weight']
                         ),
                     ),
-                    'client'  => array(
-                        MemcachedGeneric::OPT_HASH       => MemcachedGeneric::HASH_MD5,
+                    'client' => array(
+                        MemcachedGeneric::OPT_HASH => MemcachedGeneric::HASH_MD5,
                         MemcachedGeneric::OPT_PREFIX_KEY => $this->options['prefix'],
                     )
-                ));
+                )
+            );
         }
 
         return $this->memcached;
