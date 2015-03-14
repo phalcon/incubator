40a23177de2bbec9ce626ec71e5d4602fd9e1a4c
Sebastian Arrubia <sarrubia@gmail.com>
2015-03-14 17:55:56 -0300

    Fix SNIFF VIOLATIONS

--- Library/Phalcon/CLI/Console/Extended.php
+++ Library/Phalcon/CLI/Console/Extended.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Phalcon\CLI\Console;
-
 
 /**
  * Phalcon\CLI\Console\Extended
@@ -10,123 +8,110 @@ namespace Phalcon\CLI\Console;
  * @author  Sebastian Arrubia <sarrubia@gmail.com>
  *
  */
-class Extended extends \Phalcon\CLI\Console {
+class Extended extends \Phalcon\CLI\Console
+{
 
     private $tasksDir = '';
     private $documentation = array();
 
 
-    public function handle (array $arguments){
+    public function handle(array $arguments)
+    {
 
-        if( isset($arguments['task']) && in_array($arguments['task'],array('-h','--help','help')) ){
-
+        if (isset($arguments['task']) && in_array( $arguments['task'], array('-h','--help','help'))) {
             $this->setTasksDir();
             $this->createHelp();
             $this->showHelp();
-
             return;
-        }elseif(isset($arguments['action']) && in_array($arguments['action'],array('-h','--help','help'))){
-
+        } elseif (isset($arguments['action']) && in_array($arguments['action'], array('-h','--help','help'))) {
             $this->setTasksDir();
             $this->createHelp();
             $this->showTaskHelp($arguments['task']);
-
             return;
-
         }
 
         parent::handle($arguments);
     }
 
-    private function setTasksDir(){
+    private function setTasksDir()
+    {
         $config = $this->getDI()->get('config');
 
-        if( !is_dir($config['tasksDir']) ){
+        if (!is_dir($config['tasksDir'])) {
             throw new \Phalcon\CLI\Console\Exception("Invalid provided tasks Dir");
         }
 
         $this->tasksDir = $config['tasksDir'];
     }
 
-    private function createHelp(){
-
+    private function createHelp()
+    {
         $scannedTasksDir = array_diff(scandir($this->tasksDir), array('..', '.'));
 
         $config = $this->getDI()->get('config');
 
-        if( isset($config['annotationsAdapter']) ){
-
-            if( $config['annotationsAdapter'] == 'memory' ){
+        if (isset($config['annotationsAdapter'])) {
+            if ($config['annotationsAdapter'] == 'memory') {
                 $reader = new \Phalcon\Annotations\Adapter\Memory();
-            }elseif( $config['annotationsAdapter'] == 'apc' ){
+            } elseif ($config['annotationsAdapter'] == 'apc') {
                 $reader = new \Phalcon\Annotations\Adapter\Apc();
-            }else{
+            } else {
                 $reader = new \Phalcon\Annotations\Adapter\Memory();
             }
-
-        }else{
+        } else {
             $reader = new \Phalcon\Annotations\Adapter\Memory();
         }
 
-        foreach($scannedTasksDir as $taskFile){
-
+        foreach ($scannedTasksDir as $taskFile) {
             $taskClass = str_replace('.php','',$taskFile);
             $taskName  = strtolower(str_replace('Task','',$taskClass));
 
-            $this->documentation[$taskName] = array(    'description'=>array(''),
-                                                        'actions'=>array()
-            );
+            $this->documentation[$taskName] = array('description'=>array(''), 'actions'=>array());
 
             $reflector = $reader->get( $taskClass );
             $annotations = $reflector->getClassAnnotations();
 
             $methodAnnotations = $reflector->getMethodsAnnotations();
 
-            if( $annotations ){
+            if ($annotations) {
                 //Class Annotations
                 foreach ($annotations as $annotation) {
-
-                    if( $annotation->getName() == 'description' ){
+                    if ($annotation->getName() == 'description') {
                         $this->documentation[$taskName]['description'] = $annotation->getArguments();
                     }
-
                 }
                 //Method Annotations
-                if( $methodAnnotations ){
-
+                if ($methodAnnotations) {
                     foreach ($methodAnnotations as $action => $collection) {
-
                         $actionName = strtolower(str_replace('Action','',$action));
 
                         $this->documentation[$taskName]['actions'][$actionName]=array();
 
                         $actionAnnotations = $collection->getAnnotations();
 
-                        foreach($actionAnnotations as $actAnnotation){
+                        foreach ($actionAnnotations as $actAnnotation) {
                             $_anotation = $actAnnotation->getName();
-                            if($_anotation == 'description'){
+                            if ($_anotation == 'description') {
                                 $this->documentation[$taskName]['actions'][$actionName]['description'] = $actAnnotation->getArguments();
-                            }elseif($_anotation == 'param'){
+                            } elseif ($_anotation == 'param') {
                                 $this->documentation[$taskName]['actions'][$actionName]['params'][] = $actAnnotation->getArguments();
                             }
-
                         }
                     }
                 }
-
             }
         }
     }
 
-    private function showHelp(){
-
+    private function showHelp()
+    {
         $config = $this->getDI()->get('config');
         $helpOutput = PHP_EOL;
-        if( isset($config['appName']) ){
+        if (isset($config['appName'])) {
             $helpOutput .= $config['appName'] . ' ';
         }
 
-        if( isset($config['version']) ){
+        if (isset($config['version'])) {
             $helpOutput .= $config['version'];
         }
 
@@ -140,25 +125,25 @@ class Extended extends \Phalcon\CLI\Console {
         echo '           command <task> -h | --help | help'. PHP_EOL;
         echo PHP_EOL;
         echo 'Available tasks '.PHP_EOL;
-        foreach($this->documentation as $task => $doc){
+        foreach ($this->documentation as $task => $doc) {
             echo  PHP_EOL;
             echo '    '. $task . PHP_EOL ;
 
-            foreach($doc['description'] as $line){
+            foreach ($doc['description'] as $line) {
                 echo '            '.$line . PHP_EOL;
             }
         }
     }
 
-    private function showTaskHelp($taskTogetHelp){
-
+    private function showTaskHelp($taskTogetHelp)
+    {
         $config = $this->getDI()->get('config');
         $helpOutput = PHP_EOL;
-        if( isset($config['appName']) ){
+        if (isset($config['appName'])) {
             $helpOutput .= $config['appName'] . ' ';
         }
 
-        if( isset($config['version']) ){
+        if (isset($config['version'])) {
             $helpOutput .= $config['version'];
         }
 
@@ -167,42 +152,41 @@ class Extended extends \Phalcon\CLI\Console {
         echo PHP_EOL;
         echo '           command [<task> [<action> [<param1> <param2> ... <paramN>] ] ]'. PHP_EOL;
         echo PHP_EOL;
-        foreach($this->documentation as $task => $doc){
-            if( $taskTogetHelp == $task){
+        foreach ($this->documentation as $task => $doc) {
+            if ($taskTogetHelp == $task) {
                 echo  PHP_EOL;
                 echo "Task: " . $task . PHP_EOL . PHP_EOL ;
 
-                foreach($doc['description'] as $line){
+                foreach ($doc['description'] as $line) {
                     echo '  '.$line . PHP_EOL;
                 }
                 echo  PHP_EOL;
                 echo 'Available actions:'.PHP_EOL.PHP_EOL;
 
-
-                foreach($doc['actions'] as $actionName => $aDoc){
+                foreach ($doc['actions'] as $actionName => $aDoc) {
                     echo '           '.$actionName . PHP_EOL;
-                    if(isset($aDoc['description'])){
+                    if (isset($aDoc['description'])) {
                         echo '               '.implode(PHP_EOL, $aDoc['description']) . PHP_EOL;
                     }
                     echo  PHP_EOL;
-                    if( isset($aDoc['params']) && is_array($aDoc['params']) ){
+                    if (isset($aDoc['params']) && is_array($aDoc['params'])) {
                         echo '               Parameters:'.PHP_EOL;
-                        foreach($aDoc['params'] as $param){
-                            if( is_array($param) ){
+                        foreach ($aDoc['params'] as $param) {
+                            if (is_array($param)) {
                                 $_to_print = '';
-                                if(isset($param[0]['name'])){
+                                if (isset($param[0]['name'])) {
                                     $_to_print = $param[0]['name'];
                                 }
 
-                                if(isset($param[0]['type'])){
+                                if (isset($param[0]['type'])) {
                                     $_to_print .= ' ( '.$param[0]['type'].' )';
                                 }
 
-                                if(isset($param[0]['description'])){
+                                if (isset($param[0]['description'])) {
                                     $_to_print .= ' '.$param[0]['description'].PHP_EOL;
                                 }
 
-                                if( !empty($_to_print) ){
+                                if (!empty($_to_print)) {
                                     echo '                   '.$_to_print;
                                 }
                             }
@@ -213,5 +197,4 @@ class Extended extends \Phalcon\CLI\Console {
             }
         }
     }
-
 } 
