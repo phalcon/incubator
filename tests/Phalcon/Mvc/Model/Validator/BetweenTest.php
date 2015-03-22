1742e70a837ffb5869a8bb89255f9956d1c0242e
Илья Гусев <mail@igusev.ru>
2015-03-22 16:42:28 +0300

    Tests for Phalcon\Mvc\Model\Validator\Between: validate parent instance

--- tests/Phalcon/Mvc/Model/Validator/BetweenTest.php
+++ tests/Phalcon/Mvc/Model/Validator/BetweenTest.php
@@ -60,4 +60,42 @@ class BetweenTest extends \PHPUnit_Framework_TestCase
         $obj->max = $max;
         $this->assertEquals($willReturn, $obj->validation());
     }
+
+
+    /**
+     * @expectedException           \Phalcon\Mvc\Model\Exception
+     * @expectedExceptionMessage    Invalid parameter type.
+     */
+    public function testExceptionNotObject()
+    {
+        $obj = new Between(array('min' => 1, 'max' => 2, 'field' => 'position'));
+
+        $obj->validate(1);
+    }
+
+    public function testValidateInstanceOf()
+    {
+        require_once(__DIR__ . '/resources/TestBetweenModel.php');
+        $obj = new \TestBetweenModel();
+
+        $this->assertInstanceOf('Phalcon\Mvc\ModelInterface', $obj);
+    }
+
+    /**
+     * @expectedException           \Phalcon\Mvc\Model\Exception
+     * @expectedExceptionMessage    Invalid parameter type.
+     */
+    public function testValidateOtherInstance()
+    {
+        require_once(__DIR__ . '/resources/TestBetweenFail.php');
+
+        $obj = new \TestBetweenFail();
+
+        $obj->min = 1;
+        $obj->max = 3;
+        $obj->position = 4;
+
+        $this->assertNotInstanceOf('Phalcon\Mvc\ModelInterface', $obj);
+        $obj->validation();
+    }
 }
