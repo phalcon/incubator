b6f0ad6762c73e3cd73944f66882602a04ef0c87
Óscar Enríquez <abovethew4ll@gmail.com>
2015-06-03 06:44:12 +0200

    PSR2 fixes

--- Library/Phalcon/Mvc/Model/EagerLoading/QueryBuilder.php
+++ Library/Phalcon/Mvc/Model/EagerLoading/QueryBuilder.php
@@ -2,18 +2,22 @@
 
 use Phalcon\Mvc\Model\Query\Builder;
 
-final class QueryBuilder extends Builder {
-	const E_NOT_ALLOWED_METHOD_CALL = 'When eager loading relations queries must return full entities';
-	
-	public function distinct($distinct) {
-		throw new \LogicException(static::E_NOT_ALLOWED_METHOD_CALL);
-	}
+final class QueryBuilder extends Builder
+{
+    const E_NOT_ALLOWED_METHOD_CALL = 'When eager loading relations queries must return full entities';
+    
+    public function distinct($distinct)
+    {
+        throw new \LogicException(static::E_NOT_ALLOWED_METHOD_CALL);
+    }
 
-	public function columns($columns) {
-		throw new \LogicException(static::E_NOT_ALLOWED_METHOD_CALL);
-	}
+    public function columns($columns)
+    {
+        throw new \LogicException(static::E_NOT_ALLOWED_METHOD_CALL);
+    }
 
-	public function where($conditions, $bindParams = NULL, $bindTypes = NULL) {
-		return $this->andWhere($conditions, $bindParams, $bindTypes);
-	}
+    public function where($conditions, $bindParams = null, $bindTypes = null)
+    {
+        return $this->andWhere($conditions, $bindParams, $bindTypes);
+    }
 }
