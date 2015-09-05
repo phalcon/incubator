eea3a0228bec7a7c43d19cfc6502baeb627e0a9d
Serghei Iakovlev <sadhooklay@gmail.com>
2015-09-06 01:53:03 +0300

    Cleaned ExtendedYaml

--- Library/Phalcon/Config/Adapter/ExtendedYaml.php
+++ Library/Phalcon/Config/Adapter/ExtendedYaml.php
@@ -1,4 +1,21 @@
 <?php
+/*
+  +------------------------------------------------------------------------+
+  | Phalcon Framework                                                      |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2011-2015 Phalcon Team (http://www.phalconphp.com)       |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file docs/LICENSE.txt.                        |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to license@phalconphp.com so we can send you a copy immediately.       |
+  +------------------------------------------------------------------------+
+  | Authors: Alexey Stetsenko <freekzy@gmail.com>                          |
+  +------------------------------------------------------------------------+
+*/
+
 namespace Phalcon\Config\Adapter;
 
 use Phalcon\Config;
@@ -6,27 +23,32 @@ use Phalcon\Config\Exception;
 
 /**
  * Phalcon\Config\Adapter\ExtendedYaml
+ *
  * Reads yaml files and convert it to Phalcon\Config objects.
  * Unlike native Yaml adapter supports callbacks.
+ *
+ * @package Phalcon\Config\Adapter
  */
 class ExtendedYaml extends Config implements \ArrayAccess
 {
     /**
      * Class constructor.
      *
-     * @param  string                    $filePath
-     * @param  array                     $callbacks
+     * @param  string $filePath Config file path
+     * @param  array $callbacks Content handlers for YAML nodes. Associative array of YAML tag => callable mappings.
+     * @link http://php.net/manual/ru/yaml.callbacks.parse.php
+     *
      * @throws \Phalcon\Config\Exception
      */
-    public function __construct($filePath, $callbacks = array())
+    public function __construct($filePath, array $callbacks = [])
     {
         if (!extension_loaded('yaml')) {
             throw new Exception('Yaml extension not loaded');
         }
 
         $ndocs = 0;
-        if (false === $result = @yaml_parse_file($filePath, 0, $ndocs, $callbacks)) {
-            throw new Exception('Configuration file ' . $filePath . ' can\'t be loaded');
+        if (false === $result = yaml_parse_file($filePath, 0, $ndocs, $callbacks)) {
+            throw new Exception("Configuration file $filePath can't be loaded");
         }
 
         parent::__construct($result);
