68836326bef79b0a2821a871e4ec681ce16888bd
Serghei Iakovlev <sadhooklay@gmail.com>
2015-09-06 02:00:52 +0300

    Updated copyright

--- Library/Phalcon/Db/Dialect/MysqlExtended.php
+++ Library/Phalcon/Db/Dialect/MysqlExtended.php
@@ -4,7 +4,7 @@
   +------------------------------------------------------------------------+
   | Phalcon Framework                                                      |
   +------------------------------------------------------------------------+
-  | Copyright (c) 2011-2014 Phalcon Team (http://www.phalconphp.com)       |
+  | Copyright (c) 2011-2015 Phalcon Team (http://www.phalconphp.com)       |
   +------------------------------------------------------------------------+
   | This source file is subject to the New BSD License that is bundled     |
   | with this package in the file docs/LICENSE.txt.                        |
@@ -19,8 +19,6 @@
 */
 
 namespace Phalcon\Db\Dialect;
-
-use Phalcon\Db\Dialect\Mysql;
 
 /**
  * Phalcon\Db\Adapter\Dialect\MysqlExtended
