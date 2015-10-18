a3f08ff0afdf16a1909563abd37a95a305d5a069
Kachit <antoxachaos@gmail.com>
2015-10-18 20:21:26 +0300

    add db adapters factory

---
+++ Library/Phalcon/Db/Adapter/Factory.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Phalcon DB adapters factory
+ *
+ * @author Kachit
+ * @package Phalcon\Db\Adapter
+ */
+namespace Phalcon\Db\Adapter;
+
+use Phalcon\Db\Exception;
+
+class Factory
+{
+    /**
+     * Load config from file extension dynamical
+     *
+     * @param array $config
+     * @throws Exception
+     */
+    public static function load(array $config)
+    {
+        if (!isset($config['adapter'])) {
+            throw new Exception('Adapter option must be required');
+        }
+        $namespace = __NAMESPACE__ . '\\' . 'Pdo' . '\\';
+        $className = $namespace . ucfirst($config['adapter']);
+        if (!class_exists($className)) {
+            throw new Exception('Database adapter '  . $config['adapter'] . ' is not supported');
+        }
+        return new $className($config);
+    }
+}
