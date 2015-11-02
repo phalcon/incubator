6217d36e42c4530eff2789a244fdabc6ea8cb388
Kachit <antoxachaos@gmail.com>
2015-11-03 00:58:38 +0300

    refactor tests

--- tests/unit/Db/Adapter/FactoryTest.php
+++ tests/unit/Db/Adapter/FactoryTest.php
@@ -4,9 +4,21 @@ namespace Phalcon\Test\Db\Adapter;
 use Phalcon\Db\Adapter\Factory as AdaptersFactory;
 use Codeception\TestCase\Test;
 /**
- * FactoryTest
+ * \Phalcon\Test\Db\Adapter\Factory
+ * Tests for Phalcon\Db\Adapter\Factory component
  *
- * @author Kachit
+ * @copyright (c) 2011-2015 Phalcon Team
+ * @link      http://www.phalconphp.com
+ * @author    Anton Kornilov <kachit@yandex.ru>
+ * @package   Phalcon\Test\Db\Adapter
+ * @group     Db
+ *
+ * The contents of this file are subject to the New BSD License that is
+ * bundled with this package in the file docs/LICENSE.txt
+ *
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world-wide-web, please send an email to license@phalconphp.com
+ * so that we can send you a copy immediately.
  */
 class FactoryTest extends Test {
 
