59479546e6deb890a1d15bb529067066f0947a5b
Serghei Iakovlev <sadhooklay@gmail.com>
2015-11-05 23:54:05 +0200

    Improved tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -16,8 +16,8 @@ if (function_exists('mb_substitute_character')) {
 }
 
 if (extension_loaded('xdebug')) {
-    ini_set('xdebug.collect_vars', 'on');
-    ini_set('xdebug.collect_params', 4);
+    ini_set('xdebug.cli_color', 1);
+    ini_set('xdebug.collect_params', 0);
     ini_set('xdebug.dump_globals', 'on');
     ini_set('xdebug.show_local_vars', 'on');
     ini_set('xdebug.max_nesting_level', 100);
