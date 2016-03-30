de83f6428cfba3192b0b977d6e851f7bc0ed06e8
David Hubner <david.hubner@gmail.com>
2016-03-31 00:22:55 +0200

    Coding standard fixes

--- tests/unit/Validation/Validator/ConfirmationOfTest.php
+++ tests/unit/Validation/Validator/ConfirmationOfTest.php
@@ -13,27 +13,27 @@
   | obtain it through the world-wide-web, please send an email             |
   | to license@phalconphp.com so we can send you a copy immediately.       |
   +------------------------------------------------------------------------+
-  | Authors: David Hubner <david.hubner@gmail.com>                             |
+  | Authors: David Hubner <david.hubner@gmail.com>                         |
   +------------------------------------------------------------------------+
  */
 
 namespace Phalcon\Test\Validation\Validator;
 
-use Phalcon\Validation\Validator\ConfirmationOf,
-    Codeception\TestCase\Test,
-    Codeception\Util\Stub;
+use Codeception\TestCase\Test;
+use Codeception\Util\Stub;
+use Phalcon\Validation\Validator\ConfirmationOf;
 
 class ConfirmationOfTest extends Test
 {
 
     protected function _before()
     {
-        
+
     }
 
     protected function _after()
     {
-        
+
     }
 
     public function testValidateExceptionWithoutOrigField()
