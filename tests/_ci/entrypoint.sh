b51e31faa47df357d34a976da37c4652aac2e882
Serghei Iakovlev <sadhooklay@gmail.com>
2016-05-12 23:40:02 +0300

    Build Phalcon from source

---
+++ tests/_ci/entrypoint.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#
+#  Phalcon Framework
+#
+#  Copyright (c) 2016 Phalcon Team (http://www.phalconphp.com)
+#
+#  This source file is subject to the New BSD License that is bundled
+#  with this package in the file LICENSE.txt.
+#
+#  If you did not receive a copy of the license and are unable to
+#  obtain it through the world-wide-web, please send an email
+#  to license@phalconphp.com so we can send you a copy immediately.
+#
+#  Authors: Serghei Iakovlev <serghei@phalconphp.com>
+
+PURPLE="\e[0;35m"
+GREEN="\033[0;32m"
+YELLOW="\e[1;33m"
+NC="\033[0m"
+
+echo -e "\nWelcome to the Docker testing container."
+
+export PHP_EXTENSION_DIR=`php-config --extension-dir`
+echo -e "PHP extension path: ${PURPLE}${PHP_EXTENSION_DIR}${NC}\n"
+
+ln -s /ext/phalcon.so ${PHP_EXTENSION_DIR}/phalcon.so
+[[ "${TRAVIS_PHP_VERSION}" == "7" ]] || ln -s /app/tests/_ci/phalcon.ini /etc/php5/cli/conf.d/50-phalcon.ini;
+[[ "${TRAVIS_PHP_VERSION}" != "7" ]] || ln -s /app/tests/_ci/phalcon.ini /etc/php/7.0/cli/conf.d/50-phalcon.ini;
+
+export PHALCON_VERSION=`php --ri phalcon | grep "Version =" | awk '{print $3}'`
+
+echo -e "${GREEN}Phalcon${NC}     version ${YELLOW}${PHALCON_VERSION}${NC}"
+/app/vendor/bin/codecept --version
+
+/app/vendor/bin/codecept build &> /dev/null
+
+echo -e ""
+/app/vendor/bin/codecept run "${RUN_ARGS}"
+
+exit $?
+
---
+++ tests/build.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+#
+#  Phalcon Framework
+#
+#  Copyright (c) 2011-2016 Phalcon Team (http://www.phalconphp.com)
+#
+#  This source file is subject to the New BSD License that is bundled
+#  with this package in the file LICENSE.txt.
+#
+#  If you did not receive a copy of the license and are unable to
+#  obtain it through the world-wide-web, please send an email
+#  to license@phalconphp.com so we can send you a copy immediately.
+#
+#  Authors: Serghei Iakovlev <serghei@phalconphp.com>
+
+docker_bin="$(which docker.io 2> /dev/null || which docker 2> /dev/null)"
+
+[ -z "${TRAVIS_PHP_VERSION}" ] && echo "Need to set TRAVIS_PHP_VERSION variable. Fox example: 'export TRAVIS_PHP_VERSION=7'" && exit 1;
+[ -z "${PHALCON_SRC_PATH}" ] && echo "Need to set PHALCON_SRC_PATH variable. Fox example: 'export PHALCON_SRC_PATH=/home/user/src/phalcon'" && exit 1;
+[ -z "${TEST_BT_HOST}" ] && TEST_BT_HOST="incubator_beanstalkd"
+[ -z "${TRAVIS_BUILD_DIR}" ] && TRAVIS_BUILD_DIR=$(cd $(dirname "$1") && pwd -P)/$(basename "$1")
+
+RUN_ARGS="$@"
+shift
+
+cd ${PHALCON_SRC_PATH}
+
+function zephir() {
+    ${docker_bin} run -it --rm \
+        --privileged=true \
+        -e ZEND_DONT_UNLOAD_MODULES=1 \
+        -v $(pwd):/zephir \
+        phalconphp/zephir:${TRAVIS_PHP_VERSION} "/usr/local/bin/zephir $1"
+}
+
+zephir "fullclean"
+zephir "builddev"
+
+if [ ! -f $(pwd)/ext/modules/phalcon.so ]; then
+    echo "Unable to compile Phalcon."
+    exit 1;
+fi
+
+cp $(pwd)/ext/modules/phalcon.so ${TRAVIS_BUILD_DIR}/tests/_ci/phalcon.so
+
+cd ${TRAVIS_BUILD_DIR}
+
+if [ ! -f ${TRAVIS_BUILD_DIR}/tests/_ci/entrypoint.sh ]; then
+    echo "Unable locate docker entry point."
+    exit 1;
+fi
+
+chmod +x ${TRAVIS_BUILD_DIR}/tests/_ci/entrypoint.sh
+
+if [ -z ${TRAVIS} ]; then ${docker_bin} restart ${TEST_BT_HOST}; fi
+
+${docker_bin} run -it --rm \
+  --entrypoint /entrypoint.sh \
+  --privileged=true \
+  --net=incubator_default \
+  -e RUN_ARGS="${RUN_ARGS}" \
+  -e TEST_BT_HOST="${TEST_BT_HOST}" \
+  -e TRAVIS_PHP_VERSION="${TRAVIS_PHP_VERSION}" \
+  --name test-incubator-${TRAVIS_PHP_VERSION} \
+  -v ${TRAVIS_BUILD_DIR}/tests/_ci/entrypoint.sh:/entrypoint.sh \
+  -v ${TRAVIS_BUILD_DIR}/vendor:/app/vendor \
+  -v ${TRAVIS_BUILD_DIR}/codeception.yml:/app/codeception.yml \
+  -v ${TRAVIS_BUILD_DIR}/tests:/app/tests \
+  -v ${TRAVIS_BUILD_DIR}/Library:/app/Library \
+  -v ${TRAVIS_BUILD_DIR}/vendor:/app/vendor \
+  -v ${TRAVIS_BUILD_DIR}/tests/_ci/phalcon.so:/ext/phalcon.so \
+  phalconphp/php:${TRAVIS_PHP_VERSION} bash
