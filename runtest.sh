4f0216710ddab3c47c5ced10ddc8b4294d902426
Serghei Iakovlev <serghei@phalconphp.com>
2016-05-13 08:47:44 +0300

    Cleanup Docker build scripts

--- runtest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sleep 5
-
-docker run -it --rm --net=incubator_default -v $(pwd):/www klay/php-cli vendor/bin/phpcs --standard=PSR2 --colors --extensions=php --encoding=utf-8 Library/
-
-docker run -it --rm --net=incubator_default -v $(pwd):/www klay/php-cli vendor/bin/codecept build
-
-docker run -it --rm --net=incubator_default -v $(pwd):/www klay/php-cli vendor/bin/codecept run
