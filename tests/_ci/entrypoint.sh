234de65bab40b2eb2612f4180d236aaac3706620
Serghei Iakovlev <serghei@phalconphp.com>
2016-06-14 01:07:54 +0300

    Improved installing phalcon inside container

--- tests/_ci/entrypoint.sh
+++ tests/_ci/entrypoint.sh
@@ -25,9 +25,9 @@ echo -e "PHP extension path: ${PURPLE}${PHP_EXTENSION_DIR}${NC}\n"
 
 ln -s /ext/phalcon.so ${PHP_EXTENSION_DIR}/phalcon.so
 
-[[ "${TRAVIS_PHP_VERSION}" == "5.4" ]] && ln -s /app/tests/_ci/phalcon.ini /etc/php5/cli/conf.d/50-phalcon.ini;
-[[ "${TRAVIS_PHP_VERSION}" == "7" ]] || ln -s /app/tests/_ci/phalcon.ini /etc/php/${TRAVIS_PHP_VERSION}/cli/conf.d/50-phalcon.ini;
-[[ "${TRAVIS_PHP_VERSION}" != "7" ]] || ln -s /app/tests/_ci/phalcon.ini /etc/php/7.0/cli/conf.d/50-phalcon.ini;
+[[ "${TRAVIS_PHP_VERSION}" == "5.4" ]] && (rm -f /etc/php5/conf.d/50-phalcon.ini; ln -s /app/tests/_ci/phalcon.ini /etc/php5/cli/conf.d/50-phalcon.ini)
+[[ "${TRAVIS_PHP_VERSION}" == "7" ]] || (rm -f /etc/php/${TRAVIS_PHP_VERSION}/cli/conf.d/50-phalcon.ini; ln -s /app/tests/_ci/phalcon.ini /etc/php/${TRAVIS_PHP_VERSION}/cli/conf.d/50-phalcon.ini)
+[[ "${TRAVIS_PHP_VERSION}" != "7" ]] || (rm -f /etc/php/7.0/cli/conf.d/50-phalcon.ini; ln -s /app/tests/_ci/phalcon.ini /etc/php/7.0/cli/conf.d/50-phalcon.ini)
 
 export PHALCON_VERSION=`php --ri phalcon | grep "Version =" | awk '{print $3}'`
 
