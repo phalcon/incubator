1605657430122cb7572cabca63062996fd0bab10
Ben Casey <bcasey@tigerstrikemedia.com>
2016-06-15 11:47:39 +0930

    Added MongoDB Driver and a MongoDBCollection

---
+++ Library/Phalcon/Db/Adapter/MongoDB/Exception/RuntimeException.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Phalcon\Db\Adapter\MongoDB\Exception;
+
+class RuntimeException extends \MongoDB\Driver\Exception\RuntimeException implements Exception {
+}
---
+++ Library/Phalcon/Db/Adapter/MongoDB/Exception/UnexpectedValueException.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Phalcon\Db\Adapter\MongoDB\Exception;
+
+class UnexpectedValueException extends \MongoDB\Driver\Exception\UnexpectedValueException implements Exception {
+}
