34d657a50057787662b0458333a296fd1fdc13d4
Ben Casey <bcasey@tigerstrikemedia.com>
2016-07-03 00:33:48 +0930

    -- Added MongoDB Instance to docker-compose to test with
    -- Fixed bug in specifying fields found in unit testing
    
    -- Added TEST_MONGODB_HOST to tests bootstrap
    
    -- Removed duplicated line in the build arguments of build.sh
    
    -- Added a model class to use and unit tests for the following functions: save() delete() find() findById() findFirst() and aggregate()
    
    -- Removed unused `use` lines in MongoCollection.php

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -57,3 +57,6 @@ define('TEST_DB_USER', getenv('TEST_DB_USER') ?: 'root');
 define('TEST_DB_PASSWD', getenv('TEST_DB_PASSWD') ?: '');
 define('TEST_DB_NAME', getenv('TEST_DB_NAME') ?: 'incubator_tests');
 define('TEST_DB_CHARSET', getenv('TEST_DB_CHARSET') ?: 'utf8');
+
+// MongoDB
+define('TEST_MONGODB_HOST', getenv('TEST_MONGODB_HOST') ?: 'mongodb' );
