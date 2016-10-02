4ddd06b2305f47674383b39062f7eb863a515fcd
Serghei Iakovlev <serghei@phalconphp.com>
2016-10-02 20:06:50 +0300

    Update license

--- Library/Phalcon/Logger/Adapter/Firelogger.php
+++ Library/Phalcon/Logger/Adapter/Firelogger.php
@@ -1,4 +1,22 @@
 <?php
+
+/*
+  +------------------------------------------------------------------------+
+  | Phalcon Framework                                                      |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2011-2016 Phalcon Team (https://www.phalconphp.com)      |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file LICENSE.txt.                             |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to license@phalconphp.com so we can send you a copy immediately.       |
+  +------------------------------------------------------------------------+
+  | Authors: Richard Laffers <rlaffers@gmail.com>                          |
+  +------------------------------------------------------------------------+
+*/
+
 namespace Phalcon\Logger\Adapter;
 
 use Phalcon\Logger\Formatter\Firelogger as FireloggerFormatter;
@@ -9,10 +27,7 @@ use Phalcon\Logger\AdapterInterface;
  * Phalcon\Logger\Adapter\Firelogger
  * Sends messages to the Firelogger extension in Firefox.
  *
- * @link    http://firelogger.binaryage.com/
- * @version 0.1
- * @author  Richard Laffers <rlaffers@gmail.com>
- * @license The BSD 3-Clause License {@link http://opensource.org/licenses/BSD-3-Clause}
+ * @link http://firelogger.binaryage.com/
  */
 class Firelogger extends LoggerAdapter implements AdapterInterface
 {
@@ -240,7 +255,7 @@ class Firelogger extends LoggerAdapter implements AdapterInterface
             $serverHash = md5("#FireLoggerPassword#" . $this->options['password'] . "#");
             if ($clientHash !== $serverHash) { // passwords do not match
                 $this->enabled = false;
-                
+
                 if ($this->options['triggerError']) {
                     trigger_error(
                         "FireLogger passwords do not match. Have you specified correct password FireLogger extension?"
