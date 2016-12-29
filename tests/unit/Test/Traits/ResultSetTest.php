a20b32fb4639342686e6a716d55d1e027d1e8612
Phoenix Osiris <blogger@twistersfury.com>
2016-12-29 10:17:46 -0500

    DocBlock Update

--- tests/unit/Test/Traits/ResultSetTest.php
+++ tests/unit/Test/Traits/ResultSetTest.php
@@ -1,16 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Charles.Martin
- * Date: 12/13/2016
- * Time: 12:41 PM
- */
-
-namespace unit\Test\Traits;
+namespace Phalcon\Test\Test\Traits;
 
 use Phalcon\Test\Traits\ResultSet;
 use Codeception\TestCase\Test;
 
+/**
+ * \Phalcon\Test\Test\Traits\ResultSetTest
+ * Tests for Phalcon\Test\Traits\ResultSet component
+ *
+ * @copyright (c) 2011-2016 Phalcon Team
+ * @link      http://www.phalconphp.com
+ * @author    Phoenix Osiris <phoenix@twistersfury.com>
+ * @package   Phalcon\Test\Test\Traits
+ * @group     Acl
+ *
+ * The contents of this file are subject to the New BSD License that is
+ * bundled with this package in the file docs/LICENSE.txt
+ *
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world-wide-web, please send an email to license@phalconphp.com
+ * so that we can send you a copy immediately.
+ */
 class ResultSetTest extends Test
 {
     use ResultSet;
