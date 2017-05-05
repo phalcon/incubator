aa25576f1436d72c5fd5f7806ead7c6a663301c9
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2017-05-05 12:01:10 +0200

    Added collation support for aggregate

--- Library/Phalcon/Db/Adapter/MongoDB/Operation/Aggregate.php
+++ Library/Phalcon/Db/Adapter/MongoDB/Operation/Aggregate.php
@@ -267,6 +267,10 @@ class Aggregate implements Executable
         if ($this->options['useCursor']) {
             $cmd['cursor']=isset($this->options["batchSize"])?['batchSize'=>$this->options["batchSize"]]:new stdClass;
         }
+        
+        if (isset($this->options['collation'])) {
+            $cmd['collation']=$this->options['collation'];
+        }
 
         return new Command($cmd);
     }
