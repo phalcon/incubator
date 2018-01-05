2019931cbe6ca1fe56ac4495bfbba7397361309f
sergeysviridenko <sergey.v.sviridenko@gmail.com>
2018-01-05 18:20:49 +0200

    Fix undefined method in collection trait

--- tests/_support/Helper/CollectionTrait.php
+++ tests/_support/Helper/CollectionTrait.php
@@ -1,5 +1,22 @@
 <?php
 
+/*
+  +------------------------------------------------------------------------+
+  | Phalcon Framework                                                      |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2011-present Phalcon Team (https://www.phalconphp.com)   |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file LICENSE.txt.                             |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to license@phalconphp.com so we can send you a copy immediately.       |
+  +------------------------------------------------------------------------+
+  | Authors: Wojciech Ślawski <jurigag@gmail.com>                          |
+  +------------------------------------------------------------------------+
+*/
+
 namespace Helper;
 
 use Codeception\Actor;
@@ -20,7 +37,7 @@ trait CollectionTrait
     protected function setupMongo(Actor $I)
     {
         if (!extension_loaded('mongodb')) {
-            $I->markTestSkipped('mongodb extension not loaded');
+            throw new \PHPUnit_Framework_SkippedTestError('mongodb extension not loaded');
         }
 
         Di::reset();
