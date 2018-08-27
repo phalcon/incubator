477f5b875667d24573b6d25eb271c5036db85d7c
David <dav.tapia@gmail.com>
2018-08-27 15:12:49 +0200

    Update Update.php

--- Library/Phalcon/Db/Adapter/MongoDB/Operation/Update.php
+++ Library/Phalcon/Db/Adapter/MongoDB/Operation/Update.php
@@ -135,8 +135,8 @@ class Update implements Executable
             'multi' =>$this->options['multi'],
             'upsert'=>$this->options['upsert'],
         ];
-	    
-        if(isset($this->options['arrayFilters'])) {
+
+        if (isset($this->options['arrayFilters'])) {
             $updateOptions['arrayFilters'] = $this->options['arrayFilters'];
         }
 
