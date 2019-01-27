5b0cfbc4c3dac62af274a732f531a1c0134599d2
Wajdi Jurry <wjurry@arab-express.com>
2019-01-27 21:49:02 +0200

    Add new validator: ArrayInclusionIn

---
+++ Library/Phalcon/Validation/Validator/ArrayInclusionIn.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Phalcon\Validation\Validator;
+
+use Phalcon\Validation\Message;
+use Phalcon\Validation\Validator;
+use Phalcon\Validation\ValidatorInterface;
+
+class ArrayInclusionIn extends Validator implements ValidatorInterface
+{
+
+    /**
+     * Executes the validation
+     *
+     * @param \Phalcon\Validation $validation
+     * @param string $attribute
+     * @return bool
+     *
+     * @throws \Exception
+     */
+    public function validate(\Phalcon\Validation $validation, $attribute)
+    {
+        $array = $validation->getValue($attribute);
+        $domain = $this->getOption('domain');
+        $allowEmpty = $this->getOption('allowEmpty');
+
+        if ((empty($array) && !$allowEmpty) || empty($domain) || !is_array($array)) {
+            $validation->appendMessage(new Message('Invalid argument supplied', $attribute));
+            return false;
+        }
+
+        foreach ($array as $item) {
+            if (!in_array($item, $domain)) {
+                $message = $this->getOption('message', 'Values provided not exist in domain');
+                $validation->appendMessage(new Message($message, $attribute));
+                return false;
+            }
+        }
+        return true;
+    }
+}
