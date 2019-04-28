b453106dd0264d4d21a9c091418e92fbc371499e
Ruud Boon <ruud@t-boonproductions.nl>
2019-04-28 08:48:34 +0200

    Fixing apc build issue

--- tests/_ci/install_prereqs_7.sh
+++ tests/_ci/install_prereqs_7.sh
@@ -11,6 +11,8 @@
 #  obtain it through the world-wide-web, please send an email
 #  to license@phalconphp.com so we can send you a copy immediately.
 
+echo 'extension="apcu.so"' >> "$(phpenv root)/versions/$(phpenv version-name)/etc/php.ini"
+
 printf "\n" | pecl install --force apcu_bc &> /dev/null
 printf "\n" | pecl install --force igbinary &> /dev/null
 printf "\n" | pecl install --force imagick &> /dev/null
