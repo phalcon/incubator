7d7b5b96405e41f49495873a04b31961f8123029
Ruud Boon <ruud@t-boonproductions.nl>
2019-07-28 17:57:15 +0200

    merge cherry-pick

---
+++ src/Phalcon/Cache/Adapter/Aerospike.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalconphp.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Phalcon\Cache\Adapter;
+
+use Phalcon\Cache\Adapter\AdapterInterface as CacheAdapterInterface;
+use Phalcon\Storage\Adapter\Aerospike as StorageAerospike;
+
+/**
+ * Aerospike adapter
+ */
+class Aerospike extends StorageAerospike implements CacheAdapterInterface
+{
+}
